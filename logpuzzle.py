be2881c29accfd9fe59a585f4a0db7f9ee97108b
Justin_Phillips <dcoltwinchester1967@gmail.com>
2020-07-07 23:23:54 -0400

    still need to get part C working

--- logpuzzle.py
+++ logpuzzle.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python2
 """
 Log Puzzle exercise
-
 Copyright 2010 Google Inc.
 Licensed under the Apache License, Version 2.0
 http://www.apache.org/licenses/LICENSE-2.0
-
 Given an Apache logfile, find the puzzle URLs and download the images.
-
 Here's what a puzzle URL looks like (spread out onto multiple lines):
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg
 HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
@@ -26,8 +23,26 @@ def read_urls(filename):
     extracting the hostname from the filename itself, sorting
     alphabetically in increasing order, and screening out duplicates.
     """
-    # +++your code here+++
-    pass
+    urls = []
+    sorted_urls = []
+    server = ''
+    with open(filename) as f:
+        split_filename = filename.split('_')
+        server = split_filename[-1]
+        text = f.read().split(' ')
+        for string in text: 
+            if filename == 'place_code.google.com': # make place url list
+                if re.search(r'\w+-\w+\.jpg', string):
+                    urls.append('http://' + server + string)
+                    urls = list(set(urls))
+                    sorted_urls = sorted(urls)  
+            if filename == 'animal_code.google.com': # make animal url list
+                if 'puzzle' in string and string not in urls:
+                    urls.append('http://' + server + string)
+                    urls = list(set(urls))
+                    sorted_urls = sorted(urls)
+        return sorted_urls
+    
 
 
 def download_images(img_urls, dest_dir):
@@ -38,8 +53,19 @@ def download_images(img_urls, dest_dir):
     to show each local image file.
     Creates the directory if necessary.
     """
-    # +++your code here+++
-    pass
+    os.mkdir(dest_dir)  
+    imgs_string = ''
+    for i, img_url in enumerate(img_urls):
+        print("Retrieving...: ", img_url)
+        # get img and upload
+        urllib.request.urlretrieve(
+            img_url, filename=dest_dir + '/img'+str(i)+'.jpg')
+        imgs_string = imgs_string + \
+            '<img src="img' + str(i) + '.jpg">'  
+    with open(dest_dir + '/index.html', 'w') as f:
+        f.write('<html><body>' + imgs_string +
+                '</body></html>')  # make index.html
+    return
 
 
 def create_parser():
@@ -72,3 +98,4 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
